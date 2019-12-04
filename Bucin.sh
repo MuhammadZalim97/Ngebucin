f94c882a8032915827d2c52caeffaeefe3cfc4ba
MuhammadZalim97 <55618989+MuhammadZalim97@users.noreply.github.com>
2019-12-04 18:39:51 +0700

    Create Bucin.sh

---
+++ Bucin.sh
@@ -0,0 +1,141 @@
+#!/bin/sh
+
+#Recode ke Recode Gua Doa'in yang Recode Rezeki nya Di Ambil Tuhan Amin.
+
+#kalau Mau Lihat Kode Perintahnya Silahkan
+
+#ingat Belajarlah Secara Otodidak
+
+
+cyan='\e[0;36m'
+purple='\e[0;35m'
+brown='\e[0;33m'
+lightgray='\e[0;37m'
+darkgray='\e[1;30m'
+lightblue='\e[1;34m'
+lightgreen='\e[1;32m'
+lightcyan='\e[1;36m'
+lightred='\e[1;31m'
+lightpurple='\e[1;35m'
+yellow='\e[1;33m'
+white='\e[1;37m'
+
+echo -e  $white "╔═══════════╗"
+echo -e $white '║' $lightgreen "Welcome " $white'║'
+echo -e $white "║═══════════╝"
+echo -e $white '║'
+echo -e $white "║═════════════════════════════════════════╗"
+echo -e $white '║' $lightcyan "Selamat Datang Di Dunia Para Bucin " $white '  ║'
+echo -e $white '║' $yellow "Dimana Yang Hambar Akan Terasa Manis " $white '║'
+echo -e $white '║' $lightcyan  "Disini " $white '                              ║'
+echo -e $white '║' $yellow "Dimana Yang Jomblo Makin Gila " $white '       ║'
+echo -e $white '║' $lightcyan "Disini" $white '                               ║'
+echo -e $white '║' $lightred "Selamat Menyaksikan Aksi Gila" $white '        ║'
+echo -e $white '╚═════════════════════════════════════════╝'
+sleep 11
+echo -e $brown "Di mulai Dari Hitungan 1 Sampai 10"
+sleep 4
+echo -e  $lightgreen "1"
+sleep 1
+echo -e  $lightgreen "2"
+sleep 1
+echo -e  $lightgreen "3"
+sleep 1
+echo -e  $lightgreen "4"
+sleep 1
+echo -e  $lightgreen "5"
+sleep 1
+echo -e  $lightgreen "6"
+sleep 1
+echo -e  $lightgreen "7"
+sleep 1
+echo -e  $lightred "8"
+sleep 1
+echo -e  $lightred "9"
+sleep 1
+echo -e  $lightred "10"
+sleep 2
+echo -e $white "Duarr Memek"
+sleep 3
+
+clear
+
+echo -e $lightpurple " ╔══════════════╗"
+echo -e $lightgreen " ║ Pilihan Menu ║"
+echo -e $lightpurple"  ║══════════════╝"
+echo -e $lightpurple"  ║"
+echo -e $lightpurple"  ║═════════════════╗"
+echo -e $lightgreen " ║(1). BUCIN Awam  ║"
+echo -e $lightpurple"  ║═════════════════╝"
+echo -e $lightpurple"  ║═══════════════════════════╗"
+echo -e $lightgreen " ║(2). BUCIN Kids Jaman Now  ║"
+echo -e $lightpurple " ║═══════════════════════════╝"
+echo -e $lightpurple"  ║═══════════════════╗"
+echo -e $lightgreen"  ║(3). Kakek BUCIN   ║"
+echo -e $lightpurple"  ║═══════════════════╝"
+echo -e $lightpurple"  ║═══════════════════╗"
+echo -e $lightgreen"  ║(4). Ustads BUCIN  ║"
+echo -e $lightpurple"  ║═══════════════════╝"
+echo -e $lightpurple"  ║══════════════════════╗"
+echo -e $lightgreen"  ║(5). BUCIN Awam Ke 2  ║"
+echo -e $lightpurple"  ║══════════════════════╝"
+echo -e $lightpurple"  ║══════════╗"
+echo -e $lightgreen"  ║(6). Exit ║"
+echo -e $lightpurple"  ║══════════╝"
+read -p         "  ╚════⊰>" pil
+if [ $pil = "1" ]
+then
+clear
+echo -e $lightgreen "Kamu tahu gak bedanya kamu sama anjing apa ?"
+read anjing
+echo -e $lightpurple "Mau aku kasih tau gak"
+read mau
+echo -e $lightgreen "kalau anjing itu gemesin, kalo kamu itu lemesin"
+fi 
+if [ $pil = "2" ]
+then 
+clear
+echo -e $lightgreen "Kamu Tahu Gak Hal yang paling susah Di Dunia ini apa ?"
+read pdkt
+echo -e $lightpurple "Mau aku kasih tahu gak "
+read mau
+echo -e $lightgreen "Mencintaimu Setulus Hati :v"
+fi 
+if [ $pil = 3 ]
+then
+clear
+echo -e $lightgreen "Hei kamu yang disana ?"
+read tegur
+echo -e $lightpurple "Sini Dulu.."
+read kemari
+echo -e $lightgreen "Kamu pernah dengar gak kata pepatah yang satu ini' Berakit-rakit Dahulu Berenang-renang ketepian ?"
+read pernah
+echo -e $lightpurple "Kamu tahu gak' Makna yang sebenarnya apa ?"
+read gak
+echo -e $lightgreen "Cintailah  Diriku dahulu lalu akan kubahagiakan dirimu selamanya"
+fi  
+if [ $pil = 4 ]
+then
+clear
+echo -e $lightgreen "Kamu tahu gak' sesuatu yang paling susah bagi hidupku apa ?"
+read egk
+echo -e $lightpurple "Mau aku kasih tahu gak ?"
+read mau
+echo -e $lightgreen "Menjadikanmu sebagai makmum 'sholatku "
+fi
+if [ $pil = 5 ]
+then
+clear
+echo -e $lightgreen "Kamu Tahu Gak bedanya Kamu Sama Lukisan apa ?"
+read tahu
+echo -e $lightpurple "Mau aku kasih tahu gak"
+read mau
+echo -e $lightgreen "Kalau lukisan semakin lama Semakin antik' tapi kalo kamu semakin lama semakin cantik"
+fi
+if [ $pil = 6 ]
+then
+echo -e $white "Semoga Harimu Menyenangkan ^_^"
+sleep 2
+exit
+fi
+
